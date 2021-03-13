390f91a96a356f19aedb362c16c62285e47511cb
Pratibha <pratibha.jadhav3229@gmail.com>
2021-03-13 19:44:42 +0530

    intial microservice using RabbitMQ

---
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName="localhost"};
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = "Gettting started with .Net Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Sent message {0}...", message);
+
+
+            }
+
+            Console.WriteLine("Press [enter] to exit the sender App...");
+            Console.ReadLine();
+
+
+        }
+    }
+}
